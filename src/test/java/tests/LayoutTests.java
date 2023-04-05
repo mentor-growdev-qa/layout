16a0aeb3cfb041d200d09cc3e00ea8e357cab92a
william.rosendo.soares@gmail.com <william.rosendo.soares@gmail.com>
2023-04-05 19:19:30 -0300

    add devices

--- src/test/java/tests/LayoutTests.java
+++ src/test/java/tests/LayoutTests.java
@@ -1,7 +1,9 @@
 package tests;
 
 import com.galenframework.testng.GalenTestNgTestBase;
+import core.GalenTestBase;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,22 +13,11 @@ import java.io.IOException;
 
 import static java.util.Arrays.asList;
 
-public class LayoutTests extends GalenTestNgTestBase {
-
-    @Override
-    public WebDriver createDriver(Object[] objects) {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-debugging-port=9222");
-        chromeOptions.addArguments("--no-sandbox");
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        return driver;
-    }
-
-    @Test
-    public void test() throws IOException {
-        load("https://www.growdev.com.br/", 1280, 2000);
-        checkLayout("src/test/java/specs/growdevspec.spec", asList("desktop"));
+public class LayoutTests extends GalenTestBase {
+    @Test(dataProvider = "devices")
+    public void test(TestDevice device) throws IOException {
+        load("https://www.growdev.com.br");
+        checkLayout("src/test/java/specs/growdevspec.spec", device.getTags());
     }
 
 }
